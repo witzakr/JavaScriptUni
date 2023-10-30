d1c8066c4c1c31baaf6f604036625490929d6439
witzakr <wit.zakrzewski1337@gmail.com>
2023-10-30 02:48:22 +0100

    serielizing and deserilizing

--- uni assigments/guessing game/main.js
+++ uni assigments/guessing game/main.js
@@ -2,6 +2,7 @@ const prompt = require("prompt-sync")();
 const fs = require("fs");
 
 class Player {
+    
     constructor(name, age, occupation, country_of_residence) {
         this.name = name;
         this.age = age;
@@ -10,30 +11,36 @@ class Player {
     }
 }
 
-
 let players = [];
 
 const clearList = prompt("Do you want to clear the list of players? (yes/no): ");
 if (clearList.toLowerCase() === "yes") {
-    players = []; 
+    players = [];
+} 
+
+if (clearList.toLowerCase() !== "no") {
+    for (let i = 0; i < 2; i++) {
+        const name = prompt(`Enter the name of player #${i + 1}: `);
+        const age = parseInt(prompt(`Enter the age of player #${i + 1}: `));
+        const occupation = prompt(`Enter the occupation of player #${i + 1}: `);
+        const country_of_residence = prompt(`Enter the country of residence of player #${i + 1}: `);
+
+        const player = new Player(name, age, occupation, country_of_residence);
+        players.push(player);
+    }
+
+    const jsonData = JSON.stringify(players, null, 4);
+    fs.writeFileSync("players.json", jsonData);
 }
 
-for (let i = 0; i < 2; i++) {
-    const name = prompt(`Enter the name of player #${i + 1}: `);
-    const age = parseInt(prompt(`Enter the age of player #${i + 1}: `));
-    const occupation = prompt(`Enter the occupation of player #${i + 1}: `);
-    const country_of_residence = prompt(`Enter the country of residence of player #${i + 1}: `);
-
-    const player = new Player(name, age, occupation,country_of_residence);
-    players.push(player);
+try {
+    const data = fs.readFileSync("players.json");
+    players = JSON.parse(data);
+    console.log("List of players:");
+    players.forEach((player, index) => {
+        console.log(`Player ${index + 1}: Name - ${player.name}, Age - ${player.age}, Occupation - ${player.occupation}, Country of residence - ${player.country_of_residence}`);
+    });
+    console.log("Data has been saved to players.json");
+} catch (error) {
+    console.error("Error reading or displaying player data:", error);
 }
-
-const jsonData = JSON.stringify(players, null, 4);
-fs.writeFileSync("players.json", jsonData);
-
-console.log("List of players:");
-players.forEach((player, index) => {
-    console.log(`Player ${index + 1}: Name - ${player.name}, Age - ${player.age}, Occupation - ${player.occupation}, Country of residence - ${player.country_of_residence}`);
-});
-
-console.log("Data has been saved to players.json");
